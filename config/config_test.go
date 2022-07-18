f129caeb0a1527c3217bff51cdd23d7919345719
Maksym Sitarchuk <maksenius@gmail.com>
2022-07-18 17:13:46 +0300

    MEX-289 fixed lint error

--- config/config_test.go
+++ config/config_test.go
@@ -88,7 +88,8 @@ func TestParse(t *testing.T) {
 			args: args{
 				cfg: map[string]string{
 					KeyConnection: "HOSTNAME=localhost;DATABASE=testdb;PORT=50000;UID=DB2INST1;PWD=pwd",
-					KeyTable:      "gigiurhhjjejbjhhiuiuyiuyuiyiuhkjkjmhkjhjjvnbvghcgftfiuhpobjhbvbnvbnvhgfgkjjkhjkbhjvhghgfghfhgcbvjhguiyuikhjbmbvhvghftyfyrdryyyrryhhncfgfhfjfgjfgj",
+					KeyTable: "gigiurhhjjejbjhhiuiuyiuyuiyiuhkjkjmhkjhjjvnbvghcgftfiuhpobjhbvbnvbnvhgfgkjjkhjkbhjv" +
+						"hghgfghfhgcbvjhguiyuikhjbmbvhvghftyfyrdryyyrryhhncfgfhfjfgjfgj",
 					KeyPrimaryKey: "id",
 				},
 			},

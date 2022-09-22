b6b985359731634369a26bd9c3d24e49812d0cd4
Maksym Sitarchuk <maksenius@gmail.com>
2022-09-22 16:32:10 +0300

    source connector

--- connector.go
+++ connector.go
@@ -17,12 +17,11 @@ package db2
 import (
 	sdk "github.com/conduitio/conduit-connector-sdk"
 
-	"github.com/conduitio-labs/conduit-connector-db2/destination"
 	"github.com/conduitio-labs/conduit-connector-db2/source"
 )
 
 var Connector = sdk.Connector{
 	NewSpecification: Specification,
 	NewSource:        source.New,
-	NewDestination:   destination.New,
+	NewDestination:   nil,
 }
